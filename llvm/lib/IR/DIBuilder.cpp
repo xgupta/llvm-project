35fbdde0a52646e67d7752171631d4662761ef04
Chirag.Patel <chirag@raincode.com>
2020-08-06 13:40:47 +0200

    added c/c++ api to create dynamic type debug info.

--- llvm/lib/IR/DIBuilder.cpp
+++ llvm/lib/IR/DIBuilder.cpp
@@ -320,6 +320,20 @@ DIDerivedType *DIBuilder::createQualifiedType(unsigned Tag, DIType *FromTy) {
                             0, 0, std::nullopt, DINode::FlagZero);
 }
 
+DIDerivedType *DIBuilder::createDynamicType(DIType *BTy,
+                                            DIExpression *Location) {
+  return DIDerivedType::get(VMContext, dwarf::DW_TAG_dynamic_type, "", nullptr,
+                            0, nullptr, BTy, 0, 0, 0, None, DINode::FlagZero,
+                            nullptr /*ExtraData*/, Location);
+}
+
+DIDerivedType *DIBuilder::createDynamicType(DIType *BTy,
+                                            DIExpression *Location) {
+  return DIDerivedType::get(VMContext, dwarf::DW_TAG_dynamic_type, "", nullptr,
+                            0, nullptr, BTy, 0, 0, 0, None, DINode::FlagZero,
+                            nullptr /*ExtraData*/, Location);
+}
+
 DIDerivedType *
 DIBuilder::createPointerType(DIType *PointeeTy, uint64_t SizeInBits,
                              uint32_t AlignInBits,
