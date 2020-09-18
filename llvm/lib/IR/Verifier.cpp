4ca888f6872aa2ef6e0cf0f9b5921bd153d6b4e6
Chirag.Patel <chirag@raincode.com>
2020-09-18 10:39:06 +0200

    [LLVM][Verifier] Added debugInfo verifier dynamic type extra checks.

--- llvm/lib/IR/Verifier.cpp
+++ llvm/lib/IR/Verifier.cpp
@@ -1255,8 +1255,15 @@ void Verifier::visitDIDerivedType(const DIDerivedType &N) {
   }
 
   if (N.getTag() == dwarf::DW_TAG_dynamic_type) {
+<<<<<<< HEAD
     CheckDI(cast<DIExpression>(N.getLocation())->isValid(),
              "missing data_location attribute in dynamic type", &N);
+=======
+    CheckDI(N.getLocation(),
+             "missing data location attribute in dynamic type", &N);
+    CheckDI(cast<DIExpression>(N.getLocation())->isValid(),
+             "invalid data location expression in dynamic type", &N);
+>>>>>>> 113a00ce4c27 ([LLVM][Verifier] Added debugInfo verifier dynamic type extra checks.)
   }
   CheckDI(isScope(N.getRawScope()), "invalid scope", &N, N.getRawScope());
   CheckDI(isType(N.getRawBaseType()), "invalid base type", &N,
