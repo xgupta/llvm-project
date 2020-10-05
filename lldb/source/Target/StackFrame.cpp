0b21c330acdb431286a316d8c6cdae8ac8370678
Chirag.Patel <chirag@raincode.com>
2020-10-05 06:55:33 +0200

    [LLDB][StackFrame] Added check for member name lookup to reject array of structs.

--- lldb/source/Target/StackFrame.cpp
+++ lldb/source/Target/StackFrame.cpp
@@ -1220,6 +1220,10 @@ ValueObjectSP StackFrame::GetValueObjectForFrameAggregateVariable(
   if (!valobj_sp)
     return valobj_sp;
 
+  bool is_complete;
+  if (valobj_sp->GetCompilerType().IsArrayType(nullptr, nullptr, &is_complete))
+    return nullptr;
+
   ValueObjectSP result = valobj_sp->GetChildMemberWithName(name, true);
   if (result)
     return result;
