db466ca2554fdb2933ad9b4fbafad951d682a28b
Chirag.Patel <chirag@raincode.com>
2020-12-11 10:29:30 +0100

    [LLDB][ExpressionParser] bug fixed for positive int expression e.g. p move +3 to var.

--- lldb/source/Plugins/ExpressionParser/Cobol/CobolUserExpression.cpp
+++ lldb/source/Plugins/ExpressionParser/Cobol/CobolUserExpression.cpp
@@ -311,6 +311,8 @@ ValueObjectSP CobolInterpreter::VisitBasicLit(const CobolASTBasicLit *expr) {
                                      value_string.str().c_str());
     return nullptr;
   case CobolLexer::LIT_INTEGER:
+    if (value_string.front() == '+')
+      value_string = value_string.drop_front(1);
     if (value_string.getAsInteger(0, iValue)) {
       m_error.SetErrorStringWithFormat("integer conversion error %s",
                                        value_string.str().c_str());
