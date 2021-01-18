63bf7f28d949c0b337230124ad7ce602ee766052
Chirag.Patel <chirag@raincode.com>
2021-01-18 06:38:26 +0100

    [LLDB][CobolUserExpression][PLIUserExpression] fixed variable name overwriting.

--- lldb/source/Plugins/ExpressionParser/PLI/PLIUserExpression.cpp
+++ lldb/source/Plugins/ExpressionParser/PLI/PLIUserExpression.cpp
@@ -646,8 +646,12 @@ PLIUserExpression::DoExecute(DiagnosticManager &diagnostic_manager,
     return lldb::eExpressionDiscarded;
   }
 
+  result_val_sp->UpdateValueIfNeeded();
+  ValueObjectSP const_res = ValueObjectConstResult::Create(
+      target, result_val_sp->GetValue(), ConstString());
+
   result.reset(new ExpressionVariable(ExpressionVariable::eKindPLI));
-  result->m_live_sp = result->m_frozen_sp = result_val_sp;
+  result->m_live_sp = result->m_frozen_sp = const_res;
   result->m_flags |= ExpressionVariable::EVIsProgramReference;
   PersistentExpressionState *pv =
       target->GetPersistentExpressionStateForLanguage(language);
