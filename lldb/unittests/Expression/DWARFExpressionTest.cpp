5a62236879596ab34f56220f9a05f3e208f00f48
Chirag.Patel <chirag@raincode.com>
2021-02-17 06:08:47 +0100

    rebase build fix.

--- lldb/unittests/Expression/DWARFExpressionTest.cpp
+++ lldb/unittests/Expression/DWARFExpressionTest.cpp
@@ -38,7 +38,6 @@ static llvm::Expected<Scalar> Evaluate(llvm::ArrayRef<uint8_t> expr,
   std::vector<Value> stack;
   if (!DWARFExpression::Evaluate(exe_ctx, /*reg_ctx*/ nullptr, module_sp,
                                  extractor, unit, lldb::eRegisterKindLLDB,
-                                 /*initial_value_ptr*/ nullptr,
                                  /*object_address_ptr*/ nullptr, stack,
                                  result, &status))
     return status.ToError();
