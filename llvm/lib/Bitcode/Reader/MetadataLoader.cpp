5a6b0a79e1faad682501bdf68e931db2110fcce6
Kamlesh Kumar <kamleshbhalui@gmail.com>
2023-03-14 20:24:46 +0530

    replace dyn_cast with dyn_cast_or_null to handle invalid input smoothly

--- llvm/lib/Bitcode/Reader/MetadataLoader.cpp
+++ llvm/lib/Bitcode/Reader/MetadataLoader.cpp
@@ -1898,7 +1898,7 @@ Error MetadataLoader::MetadataLoaderImpl::parseOneMetadata(
                            : nullptr, // targetFuncName
          HasStaticLink ? getMDOrNull(Record[20 + OffsetB])
                        : nullptr,  // StaticLinkExpr
-        HasRcFrameBase ? getMDOrNull(Record[21 + OffsetB])
+         HasRcFrameBase ? getMDOrNull(Record[21 + OffsetB])
                            : nullptr // RcFrameBaseExpr
          ));
     MetadataList.assignValue(SP, NextMetadataNo);
