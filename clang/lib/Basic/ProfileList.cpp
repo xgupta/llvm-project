400c889a4f0e867e3e2ceee43ae5c91f62f63c4d
Shivam Gupta <shivam98.tkg@gmail.com>
2023-10-19 18:20:05 +0530

    [Clang][InstrProf] Allow absolute path in fun.list of -fprofile-list=

--- clang/lib/Basic/ProfileList.cpp
+++ clang/lib/Basic/ProfileList.cpp
@@ -139,12 +139,22 @@ std::optional<ProfileList::ExclusionType>
 ProfileList::isFileExcluded(StringRef FileName,
                             CodeGenOptions::ProfileInstrKind Kind) const {
   StringRef Section = getSectionName(Kind);
+  // Convert the input file path to its canonical (absolute) form
+  llvm::SmallString<128> CanonicalFileName(FileName);
+  llvm::sys::fs::make_absolute(CanonicalFileName);
+
   // Check for "source:<regex>=<case>"
   if (auto V = inSection(Section, "source", FileName))
     return V;
+  if (auto V = inSection(Section, "source", CanonicalFileName))
+    return V;
   if (SCL->inSection(Section, "!src", FileName))
     return Forbid;
+  if (SCL->inSection(Section, "!src", CanonicalFileName))
+    return Forbid;
   if (SCL->inSection(Section, "src", FileName))
+    return Allow;
+  if (SCL->inSection(Section, "src", CanonicalFileName))
     return Allow;
   return std::nullopt;
 }
