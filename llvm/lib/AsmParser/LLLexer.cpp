4d9d3813d1031eb0efc0326d9a713399da638965
Shivam Gupta <shivam98.tkg@gmail.com>
2024-10-03 19:30:26 +0200

    [DebugInfo] Patch 9: Add supprot for DWARF Decimal Sign Attributes and Picture String

--- llvm/lib/AsmParser/LLLexer.cpp
+++ llvm/lib/AsmParser/LLLexer.cpp
@@ -818,6 +818,10 @@ lltok::Kind LLLexer::LexIdentifier() {
   KEYWORD(versions);
   KEYWORD(memProf);
   KEYWORD(notcold);
+  KEYWORD(pic);
+  KEYWORD(digits);
+  KEYWORD(scale);
+  KEYWORD(sign);
 
 #undef KEYWORD
 
@@ -931,6 +935,7 @@ lltok::Kind LLLexer::LexIdentifier() {
   DWKEYWORD(CC, DwarfCC);
   DWKEYWORD(OP, DwarfOp);
   DWKEYWORD(MACINFO, DwarfMacinfo);
+  DWKEYWORD(DS, DwarfDecimalSign);
 
 #undef DWKEYWORD
 
