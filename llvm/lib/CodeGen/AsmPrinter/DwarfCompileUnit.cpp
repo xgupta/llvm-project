1f24c94e881d94093b01259a09bd3d84ec631d90
Shivam Gupta <shivam98.tkg@gmail.com>
2024-10-03 12:50:50 +0200

    [DebugInfo] Patch 8: Add Support for DW_OP_call2 and DW_OP_call4

--- llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
@@ -245,7 +245,7 @@ void DwarfCompileUnit::addLocationAttribute(
       continue;
 
     // Nothing to describe without address or constant.
-    if (!Global && (!Expr || !Expr->isConstant()))
+    if (!Global && (!Expr || (!Expr->isConstant() && !Expr->getNumOperands())))
       continue;
 
     if (Global && Global->isThreadLocal() &&
@@ -258,7 +258,7 @@ void DwarfCompileUnit::addLocationAttribute(
       DwarfExpr = std::make_unique<DIEDwarfExpression>(*Asm, *this, *Loc);
     }
 
-    if (Expr) {
+    if (Expr->getFragmentInfo() != std::nullopt) {
       // According to
       // https://docs.nvidia.com/cuda/archive/10.0/ptx-writers-guide-to-interoperability/index.html#cuda-specific-dwarf
       // cuda-gdb requires DW_AT_address_class for all variables to be able to
@@ -275,6 +275,17 @@ void DwarfCompileUnit::addLocationAttribute(
         }
       }
       DwarfExpr->addFragmentOffset(Expr);
+    }
+
+    // TODO: Optimize me
+    SmallVector<DIE *, 4> refs;
+    if (Expr->getNumElements()) {
+      for (const Metadata *ref : Expr->operands()) {
+        if (auto DGV = dyn_cast<DIGlobalVariableExpression>(ref)) {
+          ref = DGV->getVariable();
+        }
+        refs.push_back(getDIE((cast<DINode>(ref))));
+      }
     }
 
     if (Global) {
@@ -364,7 +375,7 @@ void DwarfCompileUnit::addLocationAttribute(
     // to detect in the verifier.
     if (DwarfExpr->isUnknownLocation())
       DwarfExpr->setMemoryLocationKind();
-    DwarfExpr->addExpression(Expr);
+    DwarfExpr->addExpression(Expr, 0, &refs);
   }
   if (Asm->TM.getTargetTriple().isNVPTX() && DD->tuneForGDB()) {
     // According to
@@ -785,12 +796,19 @@ void DwarfCompileUnit::applyConcreteDbgVariableAttributes(
     } else if (Entry->isInt()) {
       auto *Expr = Single.getExpr();
       if (Expr && Expr->getNumElements()) {
+        SmallVector<DIE *, 4> Refs;
+        for (const Metadata *ref : Expr->operands()) {
+          if (auto DGV = dyn_cast<DIGlobalVariableExpression>(ref)) {
+            ref = DGV->getVariable();
+          }
+          Refs.push_back(getDIE((cast<DINode>(ref))));
+        }
         DIELoc *Loc = new (DIEValueAllocator) DIELoc;
         DIEDwarfExpression DwarfExpr(*Asm, *this, *Loc);
         // If there is an expression, emit raw unsigned bytes.
         DwarfExpr.addFragmentOffset(Expr);
         DwarfExpr.addUnsignedConstant(Entry->getInt());
-        DwarfExpr.addExpression(Expr);
+        DwarfExpr.addExpression(Expr, 0, &Refs);
         addBlock(VariableDie, dwarf::DW_AT_location, DwarfExpr.finalize());
         if (DwarfExpr.TagOffset)
           addUInt(VariableDie, dwarf::DW_AT_LLVM_tag_offset,
@@ -926,7 +944,15 @@ void DwarfCompileUnit::applyConcreteDbgVariableAttributes(const Loc::MMI &MMI,
     else
       DwarfExpr.addMachineRegExpression(
           *Asm->MF->getSubtarget().getRegisterInfo(), Cursor, FrameReg);
-    DwarfExpr.addExpression(std::move(Cursor));
+
+    SmallVector<DIE *, 4> Refs;
+    for (const Metadata *ref : Expr->operands()) {
+      if (auto DGV = dyn_cast<DIGlobalVariableExpression>(ref)) {
+        ref = DGV->getVariable();
+      }
+      Refs.push_back(getDIE((cast<DINode>(ref))));
+    }
+    DwarfExpr.addExpression(std::move(Cursor), 0, &Refs);
   }
   if (Asm->TM.getTargetTriple().isNVPTX() && DD->tuneForGDB()) {
     // According to
