94de15559b6391001c49c092d764e98c1ca7f201
Shivam Gupta <shivam98.tkg@gmail.com>
2024-10-05 20:06:37 +0200

    [LLDB] Patch 19: Added expression evaluation support for PLI

---
+++ lldb/source/Plugins/ExpressionParser/PLI/PLILexer.cpp
@@ -0,0 +1,212 @@
+//===--   PLILexer.cpp --------------------------------------------------*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <cstring>
+
+#include "PLILexer.h"
+
+using namespace lldb_private;
+using namespace llvm;
+
+const PLILexer::KeywordMap PLILexer::m_keywords = {
+    {"&", OP_AMP},    {":", OP_COLON},    {"(", OP_LPAREN},
+    {"[", OP_LBRACK}, {"{", OP_LBRACE},   {")", OP_RPAREN},
+    {"]", OP_RBRACK}, {"}", OP_RBRACE},   {"*", OP_STAR},
+    {"+", OP_PLUS},   {"-", OP_MINUS},    {".", OP_DOT},
+    {"OF", KW_OF},    {"STG", OP_SIZEOF}, {"STORAGE", OP_SIZEOF},
+    {"=", OP_EQ},     {">", OP_GT},       {"<", OP_LT},
+    {">=", OP_GE},    {"<=", OP_LE},      {"~", OP_NOT},
+    {"^", OP_NOT},    {"<>", OP_NE}};
+
+PLILexer::PLILexer(const char *expr)
+    : m_start(expr), m_end(expr + strlen(expr)), m_last_token(TOK_INVALID, "") {
+}
+
+const PLILexer::Token &PLILexer::Lex() {
+  bool newline = false;
+  SkipWhiteSpace(newline);
+
+  if (m_start >= m_end) {
+    m_last_token.m_text = llvm::StringRef("");
+    m_last_token.m_type = TOK_EOF;
+    return m_last_token;
+  }
+
+  const char *start = m_start;
+  PLILexer::TokenType lastTok = m_last_token.m_type;
+  m_last_token.m_type = TOK_INVALID;
+  size_t offset = 0;
+  if (newline) {
+    switch (lastTok) {
+    default:
+      m_start++;
+      start++;
+      break;
+    case TOK_IDENTIFIER:
+    case LIT_FLOAT:
+    case LIT_INTEGER:
+    case LIT_STRING:
+      m_last_token.m_text = llvm::StringRef("");
+      m_last_token.m_type = OP_SEMICOLON;
+      return m_last_token;
+    }
+  }
+
+  if (IsDigit(*m_start))
+    m_last_token.m_type = DoNumber();
+  else if (*m_start == '-' && IsDigit(m_start[1]))
+    m_last_token.m_type = DoNumber();
+  else if (*m_start == '+' && IsDigit(m_start[1]))
+    m_last_token.m_type = DoNumber();
+  else if (IsOperator(*m_start))
+    m_last_token.m_type = DoOperator();
+  else {
+    switch (*m_start) {
+    // persistant variables
+    case '$':
+    default: {
+      if (IsLetterOrDigit(*m_start))
+        m_last_token.m_type = DoIdent();
+    } break;
+    case '.': {
+      if (IsDigit(m_start[1]))
+        m_last_token.m_type = DoNumber();
+      else
+        m_last_token.m_type = DoOperator();
+    } break;
+    case '"':
+    case '`':
+      m_last_token.m_type = DoString();
+      // skip quotes
+      start++;
+      offset = 1;
+      break;
+    }
+  }
+  size_t length = m_start - start;
+  if (length)
+    length -= offset;
+  m_last_token.m_text = llvm::StringRef(start, length);
+  return m_last_token;
+}
+
+PLILexer::TokenType PLILexer::DoString() {
+  if (*m_start == '`') {
+    while (++m_start < m_end) {
+      if (*m_start == '`')
+        ++m_start;
+      return LIT_STRING;
+    }
+    return TOK_INVALID;
+  }
+
+  while (++m_start < m_end) {
+    switch (*m_start) {
+    // We already have matched openning double quotation
+    case '"':
+      ++m_start;
+      return LIT_STRING;
+    case '\n':
+      return TOK_INVALID;
+    case '\\':
+      if (m_start[1] == '\n')
+        return TOK_INVALID;
+      m_start++;
+    }
+  }
+  return TOK_INVALID;
+}
+
+PLILexer::TokenType PLILexer::DoNumber() {
+  if (m_start[0] == '0' && (m_start[1] == 'x' || m_start[1] == 'X')) {
+    m_start += 2;
+    while (IsHexChar(*m_start))
+      m_start++;
+    return LIT_INTEGER;
+  }
+
+  if (m_start[0] == '+' || m_start[0] == '-') {
+    m_start++;
+  }
+
+  bool dot_ok = true;
+  bool e_ok = true;
+  while (true) {
+    while (IsDigit(*m_start))
+      ++m_start;
+    switch (*m_start) {
+    case '.':
+      if (!dot_ok)
+        return LIT_FLOAT;
+      ++m_start;
+      dot_ok = false;
+      break;
+    case 'e':
+    case 'E':
+      if (!e_ok)
+        return LIT_FLOAT;
+      dot_ok = e_ok = false;
+      ++m_start;
+      if (*m_start == '+' || *m_start == '-')
+        ++m_start;
+      break;
+    default:
+      if (dot_ok)
+        return LIT_INTEGER;
+      return LIT_FLOAT;
+    }
+  }
+}
+
+PLILexer::TokenType PLILexer::DoIdent() {
+  const char *start = m_start++;
+  while (m_start < m_end && IsLetterOrDigit(*m_start))
+    ++m_start;
+  TokenType kw = LookupKeyword(llvm::StringRef(start, m_start - start));
+  if (kw != TOK_INVALID)
+    return kw;
+  return TOK_IDENTIFIER;
+}
+
+PLILexer::TokenType PLILexer::DoOperator() {
+  size_t longestOp = (m_end - m_start);
+  if (longestOp > 8)
+    longestOp = 7;
+
+  for (size_t i = longestOp; i > 0; --i) {
+    TokenType TT = LookupKeyword(StringRef(m_start, i));
+    if (TT != TOK_INVALID) {
+      m_start += i;
+      return TT;
+    }
+  }
+  return TOK_INVALID;
+}
+
+PLILexer::TokenType PLILexer::DoWord() {
+  const char *start = m_start;
+  while ((m_start < m_end) && IsLetterOrDigit(*m_start))
+    m_start++;
+
+  TokenType TT = LookupKeyword(StringRef(m_start, m_start - start));
+  return (TT == TOK_INVALID) ? TOK_IDENTIFIER : TT;
+}
+
+PLILexer::TokenType PLILexer::LookupKeyword(StringRef KW) {
+  const auto &it = m_keywords.find(KW.upper());
+  if (it != m_keywords.end())
+    return it->second;
+  return TOK_INVALID;
+}
+
+StringRef PLILexer::LookupToken(PLILexer::TokenType TT) {
+  for (const auto &entry : m_keywords)
+    if (entry.getValue() == TT)
+      return entry.getKey();
+  return "";
+}
---
+++ lldb/source/Plugins/ExpressionParser/PLI/PLIUserExpression.h
@@ -0,0 +1,125 @@
+//===--   PLIUserExpression.h -------------------------------------------*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef liblldb_PLIUserExpression_h_
+#define liblldb_PLIUserExpression_h_
+
+#include <memory>
+
+#include "lldb/Expression/ExpressionVariable.h"
+#include "lldb/Expression/UserExpression.h"
+#include "lldb/Target/ExecutionContext.h"
+#include "lldb/Utility/Status.h"
+#include "lldb/lldb-forward.h"
+#include "lldb/lldb-private.h"
+
+#include "Plugins/ExpressionParser/PLI/PLIAST.h"
+#include "Plugins/ExpressionParser/PLI/PLIParser.h"
+
+namespace lldb_private {
+/// TODO - not used yet.
+class PLIPersistentExpressionState
+    : public llvm::RTTIExtends<PLIPersistentExpressionState,
+                               PersistentExpressionState> {
+public:
+  // LLVM RTTI support
+  static char ID;
+  PLIPersistentExpressionState();
+
+  void RemovePersistentVariable(lldb::ExpressionVariableSP variable) override;
+
+  ConstString GetNextPersistentVariableName(bool is_error = false) override;
+
+protected:
+  llvm::StringRef
+  GetPersistentVariablePrefix(bool is_error = false) const override {
+    return "$PLI";
+  }
+
+private:
+  uint32_t m_next_persistent_variable_id = 0;
+};
+
+class PLIInterpreter {
+public:
+  PLIInterpreter(ExecutionContext &exe_ctx, const char *expr);
+
+  bool Parse();
+  lldb::ValueObjectSP Evaluate(ExecutionContext &exe_ctx);
+  lldb::ValueObjectSP EvaluateStatement(const lldb_private::PLIASTStmt *s);
+  lldb::ValueObjectSP EvaluateExpr(const lldb_private::PLIASTExpr *e);
+
+  lldb::ValueObjectSP VisitBasicLit(const lldb_private::PLIASTBasicLit *e);
+  lldb::ValueObjectSP VisitIdent(const lldb_private::PLIASTIdent *e);
+  lldb::ValueObjectSP VisitUnaryExpr(const lldb_private::PLIASTUnaryExpr *e);
+  lldb::ValueObjectSP
+  VisitSelectorExpr(const lldb_private::PLIASTSelectorExpr *e);
+  lldb::ValueObjectSP
+  VisitRefModExpr(const lldb_private::PLIASTRefModifierExpr *expr);
+  lldb::ValueObjectSP
+  VisitFuncCallExpr(const lldb_private::PLIASTFuncCallExpr *expr);
+  lldb::ValueObjectSP
+  VisitAssignmentExpr(const lldb_private::PLIASTAssignmentExpr *expr);
+
+  void set_use_dynamic(lldb::DynamicValueType use_dynamic) {
+    m_use_dynamic = use_dynamic;
+  }
+
+  Status &error() { return m_error; }
+
+private:
+  ExecutionContext m_exe_ctx;
+  lldb::StackFrameSP m_frame;
+  Status m_error;
+  lldb::DynamicValueType m_use_dynamic;
+  PLIParser m_parser;
+  std::vector<std::unique_ptr<PLIASTStmt>> m_statements;
+};
+
+class PLIUserExpression : public UserExpression {
+  // LLVM RTTI support
+  static char ID;
+
+public:
+  bool isA(const void *ClassID) const override {
+    return ClassID == &ID || UserExpression::isA(ClassID);
+  }
+  static bool classof(const Expression *obj) { return obj->isA(&ID); }
+
+  PLIUserExpression(ExecutionContextScope &exe_scope, llvm::StringRef expr,
+                    llvm::StringRef prefix, SourceLanguage language,
+                    ResultType desired_type,
+                    const EvaluateExpressionOptions &options);
+
+  bool Parse(DiagnosticManager &diagnostic_manager, ExecutionContext &exe_ctx,
+             lldb_private::ExecutionPolicy execution_policy,
+             bool keep_result_in_memory, bool generate_debug_info) override;
+
+  bool CanInterpret() override { return true; }
+  bool FinalizeJITExecution(
+      DiagnosticManager &diagnostic_manager, ExecutionContext &exe_ctx,
+      lldb::ExpressionVariableSP &result,
+      lldb::addr_t function_stack_bottom = LLDB_INVALID_ADDRESS,
+      lldb::addr_t function_stack_top = LLDB_INVALID_ADDRESS) override {
+    return true;
+  }
+
+protected:
+  lldb::ExpressionResults
+  DoExecute(DiagnosticManager &diagnostic_manager, ExecutionContext &exe_ctx,
+            const EvaluateExpressionOptions &options,
+            lldb::UserExpressionSP &shared_ptr_to_me,
+            lldb::ExpressionVariableSP &result) override;
+
+private:
+  std::unique_ptr<PLIInterpreter> m_interpreter;
+};
+
+} // namespace lldb_private
+
+#endif // liblldb_PLIUserExpression_h_
