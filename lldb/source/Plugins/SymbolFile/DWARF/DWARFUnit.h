31c96903d3dd321fbfdc420fd408265f61d2cb34
Shivam Gupta <shivam98.tkg@gmail.com>
2024-10-05 20:14:43 +0200

    [LLDB] Patch 19: Added Language plugin for COBOL

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFUnit.h
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFUnit.h
@@ -216,6 +216,8 @@ public:
 
   static lldb::IdentifierCaseType IdentifierCaseFromDWARF(uint64_t val);
 
+  lldb::LanguageType GetLanguageType();
+  
   uint64_t GetDWARFLanguageType();
 
   bool GetIsOptimized();
