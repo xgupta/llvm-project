0a236ba6b7a47237da570bdd4043b0bdbbe865dc
Shivam Gupta <shivam98.tkg@gmail.com>
2024-10-05 20:14:43 +0200

    [LLDB] Patch 16: Added Language plugin for COBOL

---
+++ lldb/source/Plugins/Language/Cobol/CobolLanguage.h
@@ -0,0 +1,68 @@
+//===-- CobolLanguage.h ---------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef liblldb_CobolLanguage_h_
+#define liblldb_CobolLanguage_h_
+
+#include "llvm/ADT/StringRef.h"
+
+#include "lldb/Target/Language.h"
+#include "lldb/Utility/ConstString.h"
+#include "lldb/lldb-private.h"
+
+namespace lldb_private {
+
+class CobolLanguage : public Language {
+public:
+  CobolLanguage() = default;
+  ~CobolLanguage() override = default;
+
+  lldb::LanguageType GetLanguageType() const override {
+    return lldb::eLanguageTypeCobol85;
+  }
+
+  HardcodedFormatters::HardcodedSummaryFinder GetHardcodedSummaries() override;
+  bool DemangledNameContainsPath(llvm::StringRef path,
+                                 ConstString demangled) const override;
+  HardcodedFormatters::HardcodedSyntheticFinder
+  GetHardcodedSynthetics() override;
+
+  bool IsSourceFile(llvm::StringRef file_path) const override;
+
+  lldb::TypeCategoryImplSP GetFormatters() override;
+
+  //------------------------------------------------------------------
+  // Static Functions
+  //------------------------------------------------------------------
+  static void Initialize();
+
+  static void Terminate();
+
+  static lldb_private::Language *CreateInstance(lldb::LanguageType language);
+
+  static llvm::StringRef GetPluginNameStatic();
+
+  static void LoadCobolFormatters(lldb::TypeCategoryImplSP category_sp);
+
+  //------------------------------------------------------------------
+  // PluginInterface protocol
+  //------------------------------------------------------------------
+  llvm::StringRef GetPluginName() override;
+
+  uint32_t GetPluginVersion();
+};
+
+namespace formatters {
+
+bool RaincodeStringSummaryProvider(ValueObject &valobj, Stream &stream,
+                                   const TypeSummaryOptions &options);
+
+} // namespace formatters
+} // namespace lldb_private
+
+#endif // liblldb_CobolLanguage_h_
