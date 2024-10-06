6fef6cb3b45588b959784eb1e787f5f58a673e3c
Shivam Gupta <shivam98.tkg@gmail.com>
2024-10-06 17:56:25 +0200

    [DebugInfo] Patch 12: Add Support for Dynamic Types in LLDB and LLVM Debug Info

--- lldb/include/lldb/lldb-enumerations.h
+++ lldb/include/lldb/lldb-enumerations.h
@@ -1139,7 +1139,8 @@ FLAGS_ENUM(TypeFlags){eTypeHasChildren = (1u << 0),
                       eTypeIsComplex = (1u << 20),
                       eTypeIsSigned = (1u << 21),
                       eTypeInstanceIsPointer = (1u << 22),
-                      eTypeIsVarString = (1u << 23)};
+                      eTypeIsVarString = (1u << 23),
+                      eTypeIsDynamic = (1u << 24)};
 
 FLAGS_ENUM(CommandFlags){
     /// eCommandRequiresTarget
